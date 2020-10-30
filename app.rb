583fbc869f6721de39636552d324156cb16a4443
Alie Stone <helloaliestone@gmail.com>
2020-10-30 10:52:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,10 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do
-      params.to_s
-    end
-  # Add your post route and action below
-  post '/food' do
+    params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
 
